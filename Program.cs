c196c3e3711b702cfdd3e07f3cedcfb61e3de402
Svetlana <lankaby@yandex.ru>
2022-07-03 12:38:18 +0300

    Добавлен ответ на вторую задачу

--- Program.cs
+++ Program.cs
@@ -13,3 +13,19 @@ result = result * a;
 }
 
 Console.Write("A в степени B равно: " + result);
+
+
+
+// Задача 2. (27): Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число  ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (a > 0)
+{
+int num = a % 10;
+a = a / 10;
+sum = sum + num;
+}
+Console.WriteLine("Cумма цифр в числе равна: " + sum);
